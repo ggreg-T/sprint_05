caba04ea7e460ea471b3ebb99cb28cb7969a741c
gregory <graigtech@gmail.com>
2022-03-10 14:03:56 +0400

    mod jeudi

--- table.js
+++ table.js
@@ -1,11 +1,33 @@
+// async function render() {
+//   let reponse = await fetch("https://restcountries.com/v3.1/region/europe");
+//   let recup = await reponse.json();
+//   let affich = "";
+//   for (let i in recup) {
+//     affich += `<tr><td class="fw-bold">${recup[i].name.official}</td> <td class="text-end">${recup[i].area}</td><td class="text-end">${recup[i].population}</td><td>${recup[i].capital}</td></tr>`;
+//     // console.log(i);
+//     document.getElementById("tabb").innerHTML = affich;
+//   }
+// }
+// render();
+
 async function render() {
-  let reponse = await fetch("https://restcountries.com/v3.1/region/europe");
-  let recup = await reponse.json();
-  let affich = "";
-  for (let i in recup) {
-    affich += `<tr><td class="fw-bold">${recup[i].name.official}</td> <td class="text-end">${recup[i].area}</td><td class="text-end">${recup[i].population}</td><td>${recup[i].capital}</td></tr>`;
-    console.log(i);
-    document.getElementById("tabb").innerHTML = affich;
+  // let recup;
+  // let demande;
+  demande = await fetch("https://restcountries.com/v3.1/region/europe");
+  try {
+    if (demande.ok) {
+      recup = await demande.json();
+      console.log("tout est bon");
+      let affich = "";
+      for (let i in recup) {
+        affich += `<tr><td class="fw-bold">${recup[i].name.official}</td> <td class="text-end">${recup[i].area}</td><td class="text-end">${recup[i].population}</td><td>${recup[i].capital}</td></tr>`;
+        // console.log(i);
+        document.getElementById("tabb").innerHTML = affich;
+      }
+    }
+  } catch (e) {
+    console.error(e);
+    console.log("dans le catch");
   }
 }
 render();
